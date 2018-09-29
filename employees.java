1ae40f9b322632695e3e1481aceacd4c82255aff
Tayyaab Tanveer <42729915+t-tanveer@users.noreply.github.com>
2018-09-29 10:49:07 -0400

    Add files via upload

---
+++ employees.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package employeerecords;
+
+
+/**
+ *
+ * @author King_TT
+ */
+class employees {
+    String fnOut;
+    String lnOut;
+    String asOut;
+    String idOut;
+    String sdOut;
+
+
+
+    public employees(String firstname, String secondname, String salary, String id, String start) {
+
+
+//        throw new UnsupportedOperationException("Not yet implemented");
+    }}
+
+
